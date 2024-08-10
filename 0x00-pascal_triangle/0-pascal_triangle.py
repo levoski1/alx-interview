8aa686e22804073525008306bd168c9bdade8ed9
Ugwoke levi <soromtolevi1@gmail.com>
2024-08-10 01:51:06 +0100

    upsated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,27 +1,20 @@
-from math import factorial
+#!/usr/bin/python3
+"""
+0. Pascal's Triangle
+"""
+
 
 def pascal_triangle(n):
+    """Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
     """
-    Generate Pascal's Triangle up to the nth row.
-    
-    Args:
-    n (int): The number of rows of Pascal's Triangle to generate.
-    
-    Returns:
-    list: A list of lists of integers representing Pascal's Triangle.
-    """
-    
-
-    if n <= 0:
-        return []
-
-    triangle = []
-
-    for i in range(n):
-        row = []
-        for j in range(i + 1):
-            ncr = factorial(i) // (factorial(j) * factorial(i - j))
-            row.append(ncr)
-        triangle.append(row)
-
-    return triangle
+    res = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            res.append(level)
+    return res
